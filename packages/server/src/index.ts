168b4bb077f48f9a4a211d5188ebf97799d05f02
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-08-04 11:39:52 +0530

    Feat (logging): Add Sentry

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,39 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
-
 import sessions from "./routes/sessions";
+import * as Sentry from "@sentry/hono/bun";
+import { sentry } from "@sentry/hono/bun";
 
 const app = new Hono();
+
+app.use(
+  sentry(app, {
+    dsn: "https://603e82458afa2d84411f971dffcf51ab@o4510674526339072.ingest.de.sentry.io/4511851000299600",
+    tracesSampleRate: 1.0,
+    enableLogs: true,
+    dataCollection: {
+      // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+      // https://docs.sentry.io/platforms/javascript/guides/hono/configuration/options/#dataCollection
+      // userInfo: false,
+      // httpBodies: [],
+    },
+  }),
+);
+
+// Your routes here
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/debug-sentry", () => {
+  // Send a log before throwing the error
+  Sentry.logger.info('User triggered test error', {
+    action: 'test_error_endpoint',
+  });
+  // Send a test metric before throwing the error
+  Sentry.metrics.count('test_counter', 1);
+  throw new Error("My first Sentry error!");
+});
 
 app.onError((error, c) => {
   if (error instanceof HTTPException) {
